03d6aff08de9b4c8f95e907176a4db4e2539aa96
Fabian Kohlhaas <fabian.kohlhaas@plusserver.com>
2023-01-05 16:04:15 +0100

    Complete exercise 1.7

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,8 +1,23 @@
 import { useState } from "react"
 
-const Button = ({onClick, text}) => <button onClick={onClick}>{text}</button>
+const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
 
-const Display = ({text, value}) => <div>{text} {value}</div>
+const Display = ({ text, value }) => (
+  <div>
+    {text} {value}
+  </div>
+)
+
+const Calculation = ({ text, value }) => {
+  if (value) {
+    return (
+      <div>
+        {text} {value}
+      </div>
+    )
+  }
+  return <div>{text} 0</div>
+}
 
 const App = () => {
   const [good, setGood] = useState(0)
@@ -17,14 +32,23 @@ const App = () => {
 
   return (
     <div>
-      <h1>Give feedback</h1>
+      <h1>give feedback</h1>
       <Button onClick={handleClickGood} text={"good"} />
       <Button onClick={handleClickNeutral} text={"neutral"} />
       <Button onClick={handleClickBad} text={"bad"} />
       <h2>statistics:</h2>
-      <Display text={'good'} value={good} />
-      <Display text={'neutral'} value={neutral} />
-      <Display text={'bad'} value={bad} />
+      <Display text={"good"} value={good} />
+      <Display text={"neutral"} value={neutral} />
+      <Display text={"bad"} value={bad} />
+      <Display text={"all"} value={good + neutral + bad} />
+      <Calculation
+        text={"average"}
+        value={(good - bad) / (good + neutral + bad)}
+      />
+      <Calculation
+        text={"positive"}
+        value={(good / (good + neutral + bad)) * 100}
+      />
     </div>
   )
 }
