ba4899cc071706496c8d07e6d6cfff6499911ad8
Fabian Kohlhaas <fabian.kohlhaas@plusserver.com>
2023-01-05 17:17:40 +0100

    Complete exercise 1.11

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -5,35 +5,39 @@ const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
 const StatisticLine = ({ text, value }) => {
   if (value) {
     return (
-      <div>
-        {text} {value}
-      </div>
+      <tr>
+        <td>{text}</td>
+        <td>{value}</td>
+      </tr>
     )
   }
   return (
-    <div>
-      {text} {0}
-    </div>
+    <tr>
+      <td>{text}</td>
+      <td>{0}</td>
+    </tr>
   )
 }
 
 const Statistics = ({ good, neutral, bad }) => {
   if (good + neutral + bad > 0) {
     return (
-      <div>
-        <StatisticLine text={"good"} value={good} />
-        <StatisticLine text={"neutral"} value={neutral} />
-        <StatisticLine text={"bad"} value={bad} />
-        <StatisticLine text={"all"} value={good + neutral + bad} />
-        <StatisticLine
-          text={"average"}
-          value={(good - bad) / (good + neutral + bad)}
-        />
-        <StatisticLine
-          text={"positive"}
-          value={(good / (good + neutral + bad)) * 100}
-        />
-      </div>
+      <table>
+        <tbody>
+          <StatisticLine text={"good"} value={good} />
+          <StatisticLine text={"neutral"} value={neutral} />
+          <StatisticLine text={"bad"} value={bad} />
+          <StatisticLine text={"all"} value={good + neutral + bad} />
+          <StatisticLine
+            text={"average"}
+            value={(good - bad) / (good + neutral + bad)}
+          />
+          <StatisticLine
+            text={"positive"}
+            value={(good / (good + neutral + bad)) * 100 + "%"}
+          />
+        </tbody>
+      </table>
     )
   }
   return (
@@ -47,11 +51,8 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-
   const handleClickGood = () => setGood(good + 1)
-
   const handleClickNeutral = () => setNeutral(neutral + 1)
-
   const handleClickBad = () => setBad(bad + 1)
 
   return (
