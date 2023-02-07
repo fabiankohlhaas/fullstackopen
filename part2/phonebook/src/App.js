8fccded0cc15f5ed7fcedbdc942ffa03aed0878c
Fabian Kohlhaas <fabian.kohlhaas@plusserver.com>
2023-02-07 13:15:11 +0100

    Complete exercise 2.8

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -6,13 +6,18 @@ const App = () => {
   // The default entry is: name: 'Arto Hellas'
   // setPersons updates the state with new entrys
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { 
+      name: 'Arto Hellas',
+      number: '040-1234567'
+    }
   ]) 
 
   // State that contains the default content of the form,
   // in this case an empty string. setNewName updates the
   // content for every new input
   const [newName, setNewName] = useState('')
+
+  const [newNumber, setNewNumber] = useState('')
 
   // Function to check if a value (name) is already in the persons array
   const objectInPersons = (object) =>
@@ -24,12 +29,13 @@ const App = () => {
     persons.some(p => JSON.stringify(p) === JSON.stringify(object))
     
   // Function to control the submit behavior of the input element.
-  const addName = (event) => {
+  const addInput = (event) => {
     // Prevents the default action of submitting a form
     event.preventDefault()
     // Object that contains the input of the form field
     const phoneBookObject = {
-      name: newName
+      name: newName,
+      number: newNumber
     }
 
     // Conditional as a ternary operator instead of a if else statement
@@ -40,30 +46,34 @@ const App = () => {
     objectInPersons(phoneBookObject)
       // The alert uses a Template string, similiar to an f string in python
       // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals
-      ? alert(`${phoneBookObject.newName} is already added to phonebook`)
+      ? alert(`This combination of name: ${phoneBookObject.name} and number:${phoneBookObject.number} is already added to the phonebook`)
       // Used to create a copy of the persons array and add the new
       // entry to it. THEN the COPY is used to update the stat. 
       // DO NOT MUTATE STATE DIRECTLY!
       : setPersons(persons.concat(phoneBookObject))
 
-    // Resets the value of the input field to an empty string
+    // Resets the value of the name input field to an empty string
     setNewName('')
+    // Resets the value of the number input field to an empty string
+    setNewNumber('')
   }
-  // Updates the content of the input field for every new entry
-  const handleNameInput = (event) => {
-    // Passes the input on to the state 
-    setNewName(event.target.value)
-  }
+  // Updates the content of the name input field for every new entry
+                                  // Passes the input on to the state
+  const handleNameInput = event => setNewName(event.target.value)  
 
+  // Updates the content of the number input field for every new entry
+                                    // Passes the input on to the state
+  const handleNumberInput = event => setNewNumber(event.target.value)
+  
   return (
     <div>
       <h2>Phonebook</h2>
-      <form onSubmit={addName}>
+      <form onSubmit={addInput}>
         <div>
-          name: <input 
-          value={newName}
-          onChange={handleNameInput}
-          />
+          name: <input value={newName} onChange={handleNameInput} />
+        </div>
+        <div>
+          number: <input value={newNumber} onChange={handleNumberInput} />
         </div>
         <div>
           <button type="submit">add</button>
--- part2/phonebook/src/components/Entry.js
+++ part2/phonebook/src/components/Entry.js
@@ -1,7 +1,7 @@
 // Component to display the entrys of the phonebook as jsx paragraphs
 const Entry = ({entry}) => {
     return (
-        <p style={{margin: 0, padding:0}}>{entry.name}</p>
+        <p style={{margin: 0, padding:0}}>{entry.name} {entry.number}</p>
     )
 }
 
