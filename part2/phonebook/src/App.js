42d4d402fc5db1fa22f21ddb84443e2bab0f562e
Fabian Kohlhaas <fabian.kohlhaas@plusserver.com>
2023-02-10 18:03:25 +0100

    Complete exercise 2.12

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -7,17 +7,15 @@ import AllEntries from './components/AllEntries'
 const App = () => {
   // State that contains an array with the phonebooks entrys.
   // setPersons updates the state with new entrys
-  const [persons, setPersons] = useState([]) 
+  const [persons, setPersons] = useState([])
 
   // To fetch the date from a local JSON - server using effect hook: https://reactjs.org/docs/hooks-effect.html
   useEffect(() => {
     console.log('effect')
-    axios
-      .get('http://localhost:3001/persons')
-      .then(response => {
-        console.log('promise fulfilled')
-        setPersons(response.data)
-      })
+    axios.get('http://localhost:3001/persons').then(response => {
+      console.log('promise fulfilled')
+      setPersons(response.data)
+    })
   }, [])
 
   // Other necessary state hooks [variable, setVariable]
@@ -28,22 +26,22 @@ const App = () => {
 
   // Function to check if a value (name) is already in the persons array
   // The some() method works similar to the find() method, but returns a boolean
-  // instead of the found object: 
+  // instead of the found object:
   // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/some
   // JSON.stringify is used to compare the two objects without false results
   // More on this here: https://www.joshbritz.co/posts/why-its-so-hard-to-check-object-equality/
-  const objectInPersons = (object) =>
-    persons.some(p => JSON.stringify(p) === JSON.stringify(object))
-    
+  const objectInPersons = object => persons.some(p => JSON.stringify(p) === JSON.stringify(object))
+
   // Function to control the submit behavior of the input element.
   // prventDefault prevents the default action of submitting a form
   // phoneBookObject = object that contains the input of the form field
-  const addInput = (event) => {
+  const addInput = event => {
     event.preventDefault()
     const phoneBookObject = {
       name: newName,
       number: newNumber
     }
+    
     // Conditional as a ternary operator instead of a if else statement
     // ObjectInPersons returns either true or false
     // The expression after the queston mark is executet if true
@@ -52,38 +50,51 @@ const App = () => {
     // The alert uses a Template string, similiar to an f string in python
     // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals
     // set Persons is used to create a copy of the persons array and add the new
-    // entry to it. THEN the COPY is used to update the stat. 
+    // entry to it. THEN the COPY is used to update the stat.
     // DO NOT MUTATE STATE DIRECTLY!
-    objectInPersons(phoneBookObject)
-      ? alert(`The combination of name: (${phoneBookObject.name}) and number: (${phoneBookObject.number}) has already been added to the phonebook`)
-      : setPersons(persons.concat(phoneBookObject))
+
+    if (objectInPersons(phoneBookObject)) {
+      alert(
+        `The combination of name: (${phoneBookObject.name}) and number: (${phoneBookObject.number}) has already been added to the phonebook`
+      )
+    } else {
+      // To send the phoneBookObject to the JSON-Server
+      axios.post('http://localhost:3001/persons', phoneBookObject).then(response => {
+      console.log(response)
+      })
+      setPersons(persons.concat(phoneBookObject))
+    }
+
+    // objectInPersons(phoneBookObject)
+    //   ? alert(
+    //       `The combination of name: (${phoneBookObject.name}) and number: (${phoneBookObject.number}) has already been added to the phonebook`
+    //     )
+    //   : setPersons(persons.concat(phoneBookObject))
     // To reset the values of the input fields to an empty string
     setNewName('')
     setNewNumber('')
   }
 
   // Updates the content of theinput fields for every new entry
-  const handleNameInput = event => setNewName(event.target.value)  
+  const handleNameInput = event => setNewName(event.target.value)
   const handleNumberInput = event => setNewNumber(event.target.value)
 
-  // To update the filter on every input and set show all to false if 
+  // To update the filter on every input and set show all to false if
   // there is any input i.e. the user choose a filter
   const handleFilterInput = event => {
     setFilter(event.target.value)
-    nameFilter === ''
-      ? setShowAll(true)
-      : setShowAll(false)
+    nameFilter === '' ? setShowAll(true) : setShowAll(false)
   }
 
   // To select which entrys are to be shown. filter compares each persons name to nameFilter
   const personsToShow = showAll
     ? persons
-    : persons.filter( person => person.name.toLocaleLowerCase().startsWith(nameFilter.toLowerCase()))
-      
+    : persons.filter(person => person.name.toLocaleLowerCase().startsWith(nameFilter.toLowerCase()))
+
   return (
     <div>
       <h1>Phonebook</h1>
-      <Searchbar value={nameFilter} onChange={handleFilterInput}/>
+      <Searchbar value={nameFilter} onChange={handleFilterInput} />
       <h2>Add a new</h2>
       <form onSubmit={addInput}>
         <InputField name={'name:'} value={newName} onChange={handleNameInput} />
@@ -93,7 +104,7 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      <AllEntries persons={personsToShow}/>
+      <AllEntries persons={personsToShow} />
     </div>
   )
 }
---
+++ resources/weblinks.md
@@ -0,0 +1,5 @@
+# Weblinks
+
+## Configuration
+
+* [ESLINT and PRETTIER](https://dev.to/knowankit/setup-eslint-and-prettier-in-react-app-357b)
